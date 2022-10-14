4cd849c7db29e6816f8323ff1f361cf48a6350d8
Dasxunya <dasha.ipad.mail@gmail.com>
2022-10-14 20:13:15 +0300

    🚀сохранение информации о конкретном атрибуте (count, c_success, c_fail)🚀

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import numpy as np
 
 data_structure = {}
 headlines = []
-attributes = []
+tree = {}
 
 
 def main():
@@ -17,7 +17,8 @@ def main():
         headlines = line.split(';')[1:]
     print(f'Заголовки: {headlines}')
 
-    randoms = random.sample(range(1, 34), int(math.sqrt(145)))
+    # номера позиций атрибутов в структуре (не путать с номером атрибута = название)
+    randoms = sorted(random.sample(range(0, 31), int(math.sqrt(145))))
     print(randoms)
 
     succes, fail = 0, 0
@@ -33,6 +34,7 @@ def main():
                     fail += 1
     print(data_structure, '\n', succes, fail)
     entropy = info_T(succes, fail)
+    compare_info_x_T(randoms)
 
 
 def info_T(s, f):
@@ -41,13 +43,35 @@ def info_T(s, f):
     return entropy
 
 
-def info_x_T(attributes):
-    i = 0
+def compare_info_x_T(attributes):
+    for i in attributes:
+        info_x_T(i)
 
 
-# def file_input(filename):
-#     with open(filename, 'r', encoding='utf-8') as file:
-#         for line in file:
+def info_x_T(attribute):
+    # [(характеристика1, count, success-count, fails-count), (характеристика2, count, success-count, fails-count),...]
+    fields = {}
+    print(attribute + 1)
+    for j in data_structure:
+        if data_structure[j][attribute][1] not in fields:
+            fields.setdefault(int(data_structure[j][attribute][1]), [(0, 0, 0)])
+
+    fields_placeholder(attribute, fields)
+
+
+def fields_placeholder(a, f):
+    for i in f:
+        k, k_s, k_f = 0, 0, 0
+        for r in data_structure:
+            if int(data_structure[r][a][1]) == i and data_structure[r][32][1] == 'S':
+                k = k + 1
+                k_s = k_s + 1
+                f[i][0] = (k, k_s, k_f)
+            if int(data_structure[r][a][1]) == i and data_structure[r][32][1] == 'F':
+                k = k + 1
+                k_f = k_f + 1
+                f[i][0] = (k, k_s, k_f)
+    print(f)
 
 
 main()
