bb53acbc05d23f4ba1f9ab251a71951f8a292a46
Dasxunya <dasha.ipad.mail@gmail.com>
2022-10-16 15:20:59 +0300

    🚀help🚀

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ tree = {}
 
 
 def main():
-    global headlines, data_structure
+    global headlines, data_structure, at, data
     filename = r'C:\Users\Dasxunya\Desktop\ITMO\3\AI\AI_lab3\data\DATA.csv'
     data_csv = pd.read_csv(filename)
     for line in data_csv:
@@ -20,6 +20,7 @@ def main():
     # номера позиций атрибутов в структуре (не путать с номером атрибута = название)
     randoms = sorted(random.sample(range(0, 31), int(math.sqrt(145))))
     print(randoms)
+    at = list.copy(randoms)
 
     succes, fail = 0, 0
     for student in data_csv.values:
@@ -33,8 +34,15 @@ def main():
                 else:
                     fail += 1
     print(data_structure, '\n', succes, fail)
-    node = compare_info_x_T(randoms)
-    print(node)
+    data = data_structure
+    print(data)
+
+    splitted_d, values = split_by_attribute_values(data_structure, 8)
+    print(splitted_d.get('1'))
+    info_x_T(splitted_d.get('1'), 8)
+
+    build_tree(data_structure, randoms)
+    # позиция атрибута 8 = 9 атрибут
 
 
 def info_T(s, f):
@@ -49,37 +57,72 @@ def info_T(s, f):
     return entropy
 
 
-def compare_info_x_T(attributes):
+def split_by_attribute_values(d, b_n):
+    """Подготовка датасета с учетом значения атрибута"""
+    values = {}
+    splitted_by_values = {}
+    values.setdefault(b_n, [])
+    attr_values = prepare_data_b_n(d, b_n)
+    values[b_n] = attr_values
+    for i in attr_values:
+        splitted_by_values.setdefault(i, {})
+
+    for i in attr_values:
+        temp = {}
+        for j in d:
+            if d[j][b_n][1] == i:
+                temp.setdefault(j, d[j])
+        splitted_by_values[i] = temp
+
+    print(values, splitted_by_values)
+    return splitted_by_values, values
+
+
+def prepare_data_b_n(structure, b_n):
+    """Получение информации о значениях в конкретном узле"""
+    global info_x
+    fields = []
+    for j in structure:
+        if structure[j][b_n][1] not in fields:
+            fields.append(structure[j][b_n][1])
+    return fields
+
+
+def compare_info_x_T(d, attributes):
     """Выбор атрибута с наилучшим информационным приростом"""
+
     global opt_a
     max_entropy = 0
+    counter = 0
+    fields_list = [0 for j in range(len(attributes))]
     for i in attributes:
-        compare = info_x_T(i)
+        compare = info_x_T(d, i)
+        fields_list[counter] = compare[2]
+        counter += 1
         if compare[0] > max_entropy:
             max_entropy = compare[0]
             opt_a = compare[1]
-    return opt_a + 1
+
+    return opt_a
 
 
-def info_x_T(attribute):
-    # {характеристика1: [(count, success-count, fails-count)], характеристика2:
-    # [(count, success-count, fails-count), ...}
+def info_x_T(splitted_d, attr):
+    # splitted_d = {student1: [(attr1, value1), (attr2, value2), ()]), student2: ...}
     global info_x
     fields = {}
-    print(attribute + 1)
-    for j in data_structure:
-        if data_structure[j][attribute][1] not in fields:
-            fields.setdefault(int(data_structure[j][attribute][1]), [(0, 0, 0)])
-
-    fields_placeholder(attribute, fields)
+    for j in splitted_d:
+        if splitted_d[j][attr][1] not in fields:
+            fields.setdefault(int(splitted_d[j][attr][1]), [(0, 0, 0)])
+    fields_placeholder(attr, fields)
     info_x = 0
     for i in fields:
-        info_x = info_x + (fields[i][0][0] / len(data_structure)) * info_T(fields[i][0][1], fields[i][0][2])
-    print(info_x)
-    return info_x, attribute
+        info_x = info_x + (fields[i][0][0] / len(splitted_d)) * info_T(fields[i][0][1], fields[i][0][2])
+    print(f'для атрибута на позиции {attr} значения {fields} энтропия =', info_x)
+    return info_x, attr, fields
 
 
 def fields_placeholder(a, f):
+    """Подсчет количества успехов и неудач среди значения атрибута"""
     for i in f:
         k, k_s, k_f = 0, 0, 0
         for r in data_structure:
@@ -91,10 +134,20 @@ def fields_placeholder(a, f):
                 k = k + 1
                 k_f = k_f + 1
                 f[i][0] = (k, k_s, k_f)
+    return f
 
 
-def build_tree():
-    i = 0
+def build_tree(d, attrs, node=None, order=0):
+    # {узел1: {ребро1: дочерний_узел1, ребро2: дочерний_узел2}, узел2:...}
+    # while node != 'S' or node != 'F':
+        if order == 0:
+            best_node = compare_info_x_T(d, attrs)  # получили оптимальную вершину
+            s_b_v, v = split_by_attribute_values(d, best_node)  # получили информацию о значениях вершины и разбили мн-во на подм-ва(s_b_v)
+            print(v[best_node], '\n', best_node)
+            for i in v[best_node]:
+                print(i)
+        # else:
+        #
 
 
 main()
